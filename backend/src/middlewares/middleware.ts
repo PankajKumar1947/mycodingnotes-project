b48b38ccfa72e55ce526d17e9e339d24d5ad8345
Pankaj Kumar <pankaj.ky3007@gmail.com>
2024-04-27 12:59:56 +0530

    middleware for auth created

---
+++ backend/src/middlewares/middleware.ts
@@ -0,0 +1,23 @@
+import { verify } from "hono/jwt";
+
+export const auth=async(c:any,next:any)=>{
+    const authHeader=c.req.header("authorization") || "";
+    try{
+        const user=await verify(authHeader,c.env.JWT_SECRET);
+
+        if(user){
+            c.set("userId",user.id);
+            await next();
+        }else{
+            c.status(403);
+            return c.json({
+                message: "You are not logged in"
+            })
+        }
+    }catch(error){
+        c.status(403);
+        return c.json({
+            message: "You are not logged in"
+        })
+    }
+}
